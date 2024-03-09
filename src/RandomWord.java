77f6c8a24914f78a3a7cce894addcd53dd98c460
Ali-Sra <150520460+Ali-Sra@users.noreply.github.com>
2024-03-09 17:31:48 +0100

    Initial commit

---
+++ src/RandomWord.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomWord {
+    public static String get() {
+        Path filePath = Paths.get("wordlist");
+        if (Files.exists(filePath)) {
+            List<String> lines = null;
+            try {
+                lines = Collections.unmodifiableList(Files.readAllLines(filePath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert lines != null;
+            return lines.get((int) (Math.random() * lines.size()) + 1);
+        }
+        else {
+            return "Test";
+        }
+    }
+
+}
+
